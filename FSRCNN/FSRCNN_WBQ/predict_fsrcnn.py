f937a3c284e737a179da0886eea729cb455b8d45
Fi-past <952127918@qq.com>
2021-12-04 20:16:43 +0800

    对三通道同时处理，最终拼合为一个放大的图片

--- FSRCNN/FSRCNN_WBQ/predict_fsrcnn.py
+++ FSRCNN/FSRCNN_WBQ/predict_fsrcnn.py
@@ -7,29 +7,44 @@ import matplotlib.pyplot as plt
 
 # 准备修改一下，仿照那个作者的模式
 # crop_size = 224
-imgPath='./data/my/1.jpg'
-model_path='./model/SRCNN_model_torch14_22.pth'
+imgPath='F:\\Desktop\\All\\language\\VSCODEWORKSPACE\\Python\\Class\\计算机视觉\\SuperResolution\\SRCNN\\SRCNN-WBQ\\data\\my\\1.jpg'
+model_path='./model/FSRCNN_model_torch1_4_25_8.pth'
 
 with torch.no_grad():
     show=ToPILImage()
 
     target_transform = transforms.Compose([
-                        # transforms.Resize([crop_size, crop_size]),
                         transforms.ToTensor(),
-                        # transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5]),
                         ])
 
     img = Image.open(imgPath).convert('YCbCr')
+
+    height = (img.size[0]//2)*2
+    width = (img.size[1]//2)*2
+
     y, cb, cr = img.split()
 
-    input = target_transform(y).view(1, -1, y.size[1], y.size[0])  # we only work with the "Y" channel
+    # cb=cb.resize((height,width))
+    # cr=cr.resize((height,width))
+    # y=y.resize((height//2,width//2))
 
     RSCNN_model = torch.load(model_path,map_location='cpu')
     RSCNN_model.eval()
 
-    out = RSCNN_model(input)
-    out=show(torch.squeeze(out))
-    out_img = Image.merge('YCbCr', [out, cb, cr]).convert('RGB')
+    input_y = target_transform(y).view(1, -1, y.size[1], y.size[0])  # we only work with the "Y" channel
+    out_y = RSCNN_model(input_y)
+    out_y=show(torch.squeeze(out_y))
+
+    cb = target_transform(cb).view(1, -1, cb.size[1], cb.size[0])  
+    cb = RSCNN_model(cb)
+    cb=show(torch.squeeze(cb))
+
+    cr = target_transform(cr).view(1, -1, cr.size[1], cr.size[0])  
+    cr = RSCNN_model(cr)
+    cr=show(torch.squeeze(cr))
+
+
+    out_img = Image.merge('YCbCr', [out_y, cb, cr]).convert('RGB')
     
 
     input=img
